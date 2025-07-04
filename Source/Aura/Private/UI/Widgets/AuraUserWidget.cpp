5489490eb78c6cc7d98d9b47cd85150b513ad719
William Marietta <wemarietta@gmail.com>
2025-07-04 10:26:46 -0400

    Created base widget class and widget controller class.

---
+++ Source/Aura/Private/UI/Widgets/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright William Marietta
+
+
+#include "UI/Widgets/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	OnWidgetControllerSet();
+}
